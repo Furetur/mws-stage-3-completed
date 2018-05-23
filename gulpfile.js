28b567efe8e5644e543ee522a681a5ccd9a1e91a
furetur <furetur@gmail.com>
2018-05-23 20:05:53 +0500

    pwa requirements are met

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,29 @@ const cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 const babelMinify = require('gulp-babel-minify');
 const gzip = require('gulp-gzip');
+const gm = require('gulp-gm');
+const rename = require('gulp-rename');
+
+
+function _copy(filename, foldername, additionalStep) {
+    const sourcePath = foldername? `${foldername}/${filename}` : filename;
+    const destPath = foldername? '/' + foldername : '';
+
+    if (additionalStep) {
+        return gulp.src(`./src/${sourcePath}`)
+            .pipe(additionalStep())
+            .pipe(gulp.dest(`./dist${destPath}`));
+    }
+
+    return gulp.src(`./src/${sourcePath}`)
+        .pipe(gulp.dest(`./dist${destPath}`));
+
+}
+
+
+function _gzip(filename, foldername) {
+    return _copy(filename, foldername, gzip);
+}
 
 
 gulp.task('minify-html', () => {
@@ -14,6 +37,17 @@ gulp.task('minify-html', () => {
         .pipe(gzip())
         .pipe(gulp.dest('./dist'));
 });
+
+
+gulp.task('html:copy', () => {
+    return _copy('*.html');
+});
+
+gulp.task('html:gzip', () => {
+    return _gzip('*.html');
+});
+
+gulp.task('html', ['html:copy', 'html:gzip']);
 
 gulp.task('minify-css', () => {
     return gulp.src(['./src/css/styles.css', './src/css/responsive.css'])
@@ -52,6 +86,33 @@ gulp.task('js-restaurant', () => {
         .pipe(gzip())
         .pipe(gulp.dest('./dist/js'));
 });
+
+gulp.task('sw:copy', () => {
+    return gulp.src('./src/sw.js')
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('sw:gzip', () => {
+    return gulp.src('./src/sw.js')
+        .pipe(gzip())
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('icon-192', () => {
+    return gulp.src('./src/icon.png')
+        .pipe(gm(gmfile => gmfile.resize(192)))
+        .pipe(rename('icon-192.png'))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('icon-512', () => {
+    return gulp.src('./src/icon.png')
+        .pipe(gm(gmfile => gmfile.resize(512)))
+        .pipe(rename('icon-512.png'))
+        .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('sw', ['sw:copy', 'sw:gzip']);
 
 gulp.task('minify-js', ['js-main', 'js-restaurant']);
 
